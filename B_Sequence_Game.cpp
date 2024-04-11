d82ac047daee055009af8a3d6ac8f18cf6adff10
merja-shourov <merjashourov13@gmail.com>
2024-04-11 14:03:32 +0600

    eid

---
+++ B_Sequence_Game.cpp
@@ -0,0 +1,32 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(void){
+    int t;
+    cin >> t;
+
+    while (t--)
+    {
+        int n;
+        cin >> n;
+        int arr[n];
+        for( int i=0; i<n; i++ ){
+            cin >> arr[i];
+        }
+        
+        vector<int> ans;
+        ans.push_back(arr[0]);
+        for( int i=1; i<n; i++ ){
+            if( arr[i] >= arr[i-1]) ans.push_back(arr[i]);
+            else{
+                ans.push_back(arr[i]); ans.push_back(arr[i]);
+            }
+        }
+
+        cout << ans.size() << endl;
+        for( auto x: ans ) cout << x << " ";
+        cout << endl;
+    }
+    
+    return 0;
+}
